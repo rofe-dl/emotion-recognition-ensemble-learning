8fc637fe9426448825241c3fa913377e25bafa70
rofe-dl <rafidul258@gmail.com>
2021-05-11 22:58:03 +0600

    first commit

---
+++ demo.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+import webbrowser
+
+r1 = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('speak now')
+    r1.adjust_for_ambient_noise(source=source)
+    audio = r1.listen(source)
+
+try:
+    print (r1.recognize_google(audio))
+except sr.UnknownValueError:
+    print("Could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from google", e)
+
