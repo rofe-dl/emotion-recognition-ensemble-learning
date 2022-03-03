c95588a5d8591abbf959b7b061bbb0e20aa272a6
rofe-dl <rafidul258@gmail.com>
2022-03-03 21:32:07 +0600

    added speech feature extraction

---
+++ process_dataset/speech_features.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from soundfile import SoundFile
+import numpy as np
+import librosa
+import pickle
+
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('config.ini')
+
+def get_speech_features():
+    with open('speech_features_separated.pkl', 'rb') as f:
+        features = pickle.load(f)
+    
+    return features
+
+def make_speech_features():
+    df = pd.read_csv(config['Dataset']['dataset_details_location'])
+
+    file_list = df['path'].tolist()
+    emotions = df['emotion'].tolist()
+
+    mfccss, chromas, mels, contrasts, tonnetzs, y= ([] for i in range(6))
+    # X, y = [], []
+
+    for index, file_name in enumerate(file_list):
+        speech_features = _extract_features(config['Dataset']['dataset_location'] + file_name)
+
+        mfccss.append(speech_features[0])
+        chromas.append(speech_features[1])
+        mels.append(speech_features[2])
+        contrasts.append(speech_features[3])
+        tonnetzs.append(speech_features[4])
+
+        # X.append(speech_features)
+        y.append(emotions[index])
+        print('On file number ', index + 1, '/10039')
+
+    # features = (X, y)
+    features = (mfccss, chromas, mels, contrasts, tonnetzs, y)
+    with open('speech_features_separated.pkl', 'wb') as f:
+        pickle.dump(features, f)
+
+def _extract_features(file_name):
+
+    with SoundFile(file_name) as sound_file:
+        audio = sound_file.read(dtype="float32")
+        sample_rate = sound_file.samplerate
+
+        stft = np.abs(librosa.stft(audio))
+        # result = np.array([])
+
+        mfccs = np.mean(librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40).T, axis=0)
+        chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+        mel = np.mean(librosa.feature.melspectrogram(y=audio, sr=sample_rate).T,axis=0)
+        contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+        tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(audio), sr=sample_rate).T,axis=0)
+
+        # result = np.hstack((result, mfccs, chroma, mel, contrast, tonnetz))
+
+    return mfccs, chroma, mel, contrast, tonnetz
+    # return result
+
+# make_speech_features()
+# get_speech_features()
