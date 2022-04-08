818e29accbcea5598729f48b30cae83bdac34e41
rofe-dl <rafidul258@gmail.com>
2022-04-08 17:10:31 +0600

    hard and soft cross

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from process_dataset import speech_features, text_features
 from ensemble import StackEnsemble, VoteEnsemble, BlendEnsemble
 import numpy as np
 import pandas as pd
+import pickle
 
 def main():
 
@@ -30,24 +31,59 @@ def main():
     x_train1, x_test1, y_train1, y_test1 = speech_features.get_train_test()
     x_train2, x_test2, y_train2, y_test2 = text_features.get_train_test()
 
-    speech_model.fit(x_train1, y_train1)
-    speech_result = speech_model.predict(x_test1, proba=True)
+    # speech_model.fit(x_train1, y_train1)
+    # with open('trained_models/speech_stack.pkl', 'wb') as f:
+    #     pickle.dump(speech_model, f)
 
-    text_model.fit(x_train2, y_train2)
-    text_result = text_model.predict(x_test2, proba=True)
+    with open('trained_models/speech_stack.pkl', 'rb') as f:
+        speech_model = pickle.load(f)
+    probas_speech = speech_model.predict(x_test1, proba=True)
 
-    probas = (speech_result + text_result) / 2
+    # text_model.fit(x_train2, y_train2)
+    # with open('trained_models/text_stack.pkl', 'wb') as f:
+    #     pickle.dump(text_model, f)
 
+    with open('trained_models/text_stack.pkl', 'rb') as f:
+        text_model = pickle.load(f)
+    probas_text = text_model.predict(x_test2, proba=True)
+
+    avg_probas = (probas_speech + probas_text) / 2
     emotions = ['ang', 'hap', 'neu', 'sad']
+    #             0      1      2      3
+
     result = []
 
-    for proba in probas:
-        proba = list(proba)
-        max_proba = max(proba)
-        max_index = proba.index(max_proba)
+    for proba_speech, proba_text in zip(probas_speech, probas_text):
+        avg_proba = (proba_speech + proba_text) / 2
+        max_index_speech, max_index_text = np.argmax([proba_speech, proba_text], axis=1)
+
+        if max_index_speech != max_index_text:
+            # speech model -> good at sad(3), neutral(2)
+            # text model -> good at ang(0), hap(1)
+
+            if (max_index_speech in [2, 3] and max_index_text in [0, 1]):
+                # both giving max proba at the emo they're good at, then average their probas
+                max_index = np.argmax(avg_proba)
+
+            elif max_index_speech in [2, 3] and max_index_text in [2, 3]:
+                # only speech giving max proba at the emo they good at
+                max_index = max_index_speech
+            
+            elif max_index_text in [0, 1] and max_index_speech in [0, 1]:
+                # only text giving max proba at the emo they good at
+                max_index = max_index_text
+            else:
+                max_index = np.argmax(avg_proba)
+
+        else:
+            max_index = max_index_speech
+
         result.append(emotions[max_index])
 
-    result = np.array(result)
+    # max_indices = np.argmax(avg_probas, axis=1)
+    # for index in max_indices:
+    #     result.append(emotions[index])
+
     print(classification_report(y_test1, result))
 
     # df1 = pd.DataFrame(speech_model.predict(x_test1, proba=True), columns=speech_model.inner.classes_)
