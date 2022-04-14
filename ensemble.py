2ddcce3060d05ef95dcbcbaa166c0c603e28211d
rofe-dl <rafidul258@gmail.com>
2022-04-15 03:32:49 +0600

    cross validate function on all custom ensembles

--- ensemble.py
+++ ensemble.py
@@ -5,6 +5,9 @@ import pickle
 
 from sklearn.model_selection import cross_val_predict, train_test_split
 from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_extraction.text import CountVectorizer
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -19,8 +22,6 @@ class Ensemble():
             pickle.dump(self, f)
 
     def cross_validate(self, x_speech, x_text, y, cv):
-
-        # wrong impementation, does overfitting because of refit
 
         if len(x_speech) < cv:
             raise ValueError("Dataset is too small for k")
@@ -69,43 +70,85 @@ class VoteEnsemble(Ensemble):
         self.speech_models = speech_models
         self.text_models = text_models
         self.type = type
-        self.voter_speech = VotingClassifier(
-            estimators=self.speech_models, 
-            voting=self.type,
-            verbose=True,
-            n_jobs=-1
-        )
-        self.voter_text = VotingClassifier(
-            estimators=self.text_models, 
-            voting=self.type,
-            verbose=True,
-            n_jobs=-1
-        )
+
+        if type == 'soft':
+            self.voter_speech = VotingClassifier(
+                estimators=self.speech_models, 
+                voting=self.type,
+                verbose=True,
+                n_jobs=-1
+            )
+            self.voter_text = VotingClassifier(
+                estimators=self.text_models, 
+                voting=self.type,
+                verbose=True,
+                n_jobs=-1
+            )
 
     def fit(self, x_speech, x_text, y):
+        if self.type == 'soft':
+            self.voter_speech.fit(x_speech, y)
+            self.voter_text.fit(x_text, y)
+        else:
+            for name, model in self.speech_models:
+                print(f"Training {name} (Speech) ...")
+                model.fit(x_speech, y)
 
-        self.voter_speech.fit(x_speech, y)
-        self.voter_text.fit(x_text, y)
+            for name, model in self.text_models:
+                print(f"Training {name} (Text) ...")
+                model.fit(x_text, y)
 
     def predict(self, x_speech, x_text, proba=False):
+        if self.type == 'soft':
+            probas_speech = self.voter_speech.predict_proba(x_speech)
+            probas_text = self.voter_text.predict_proba(x_text)
 
-        probas_speech = self.voter_speech.predict_proba(x_speech)
-        probas_text = self.voter_text.predict_proba(x_text)
+            avg_probas = (probas_speech + probas_text) / 2
 
-        avg_probas = (probas_speech + probas_text) / 2
+            if proba:
+                return avg_probas
 
-        if proba:
-            return avg_probas
+            emotions = ['ang', 'hap', 'neu', 'sad']
+            result = []
 
-        emotions = ['ang', 'hap', 'neu', 'sad']
-        result = []
+            max_indices = np.argmax(avg_probas, axis=1)
+            for index in max_indices:
+                result.append(emotions[index])
 
-        max_indices = np.argmax(avg_probas, axis=1)
-        for index in max_indices:
-            result.append(emotions[index])
+        else:
+
+            if proba:
+                raise ValueError("When using hard voting, proba can't be True")
+
+            all_predictions = []
+            for name, model in self.speech_models:
+                all_predictions.append(list(model.predict(x_speech)))
+
+            for name, model in self.text_models:
+                all_predictions.append(list(model.predict(x_text)))
+
+            all_predictions = list(zip(*all_predictions))
+            result = self._find_majority(all_predictions)
 
         return result
 
+    def _find_majority(self, all_predictions):
+
+        result = []
+        for prediction in all_predictions:
+            max_count = 0
+            max_emotion = prediction[0]
+
+            for emotion in prediction:
+                count = prediction.count(emotion)
+                
+                if count > max_count:
+                    max_count = count
+                    max_emotion = emotion
+            
+            result.append(max_emotion)
+
+        return result
 
 class BlendEnsemble(Ensemble):
 
