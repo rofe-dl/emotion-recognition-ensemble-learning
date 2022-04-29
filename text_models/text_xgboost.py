8dab2ca5742e11be442c14a806b0928323347b63
rofe-dl <rafidul258@gmail.com>
2022-04-29 15:01:46 +0600

    xgboost text retuned

--- text_models/text_xgboost.py
+++ text_models/text_xgboost.py
@@ -4,7 +4,10 @@ from xgboost import XGBClassifier
 from sklearn.metrics import classification_report
 
 def get_xgb():
-    return XGBClassifier(random_state=42, tree_method='gpu_hist')
+    # return XGBClassifier(random_state=42, tree_method='gpu_hist')
+    return XGBClassifier(**{
+        'colsample_bytree': 0.8, 'gamma': 0.2, 'learning_rate': 0.1, 'max_depth': 3, 'n_estimators': 500, 'reg_lambda': 0.001, 'subsample': 0.4, 'random_state': 42
+    })
 
 def main():
     xgb = get_xgb()
