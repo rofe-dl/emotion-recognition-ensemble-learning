af5f76885a3cd274d822d71702ab3d6fc8bbb23f
rofe-dl <rafidul258@gmail.com>
2022-04-30 20:59:17 +0600

    readme and merged text feature files

--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Emotion Recognition from Speech & Text using Ensemble Learning
 
+Code for our thesis (CSE400) where we tried multimodal emotion recognition from speech and lexical features using an ensemble of different classifiers on the IEMOCAP dataset. For that, we tried different hetergoneneous ensemble learning techniques to compare and find the best ensemble. We covered the following techniques:
+
+* Hard Voting
+* Soft Voting
+* Stacking
+* Blending
+
+We have 6 models trained on speech data, and 6 models trained on text data. The findings of all 12 models are combined using the ensemble methods. The results are found in `main_all_models.ipynb`.
+
 ## Setup Instructions
 
-1. Clone the repository with 
-    ```
-    git clone git@github.com:rofe-dl/CSE400-NLP.git
-    ```
+1. Clone the repository
+
 1. `cd` to the codebase and create a virtual environment and activate it.
     ```
-    cd CSE400-NLP
+    cd emotion-recognition-ensemble-learning
     ```
     * For Linux
     ```
@@ -23,6 +30,24 @@
 1. Install necessary libraries
     ```
     pip install -r requirements.txt
-    pip install PyAudio-0.2.11-cp37-cp37m-win_amd64.whl #depends on your python version (37, 38 or 39)
+    pip install PyAudio-0.2.11-cp37-cp37m-win_amd64.whl # (OPTIONAL) depends on your python version (37, 38 or 39)
     ```
+1. Download the IEMOCAP dataset by submitting a request from [here](https://sail.usc.edu/iemocap/iemocap_release.htm). Will take 1-3 days for them to email you.
+1. Make a folder named `data` in the project directory and put the dataset there. Rename the folder to `IEMOCAP_dataset`
+1. Process the dataset and extract features into the data folder
+    ```
+    python3 -m process_dataset.speech_features
+    python3 -m process_dataset.text_features
+    ```
+1. Run either of the main .ipynb files for all_models (ensemble all models) or best_models (to ensemble the best 4 models from each speech and text)
 
+## Individual Models
+
+To test out the performance of each model individually, just run their respective file as a module. For example
+```
+python3 -m speech_models.speech_logistic_regression
+```
+
+---
+
+IEMOCAP metadata was obtained from [here](https://www.kaggle.com/datasets/samuelsamsudinng/iemocap-emotion-speech-database).
--- process_dataset/text_features.py
+++ process_dataset/text_features.py
@@ -1,10 +1,8 @@
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-
-import string
+import csv
+import os
 import pandas as pd
-import pickle
-import numpy as np
+import unicodedata
+import re
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
@@ -33,3 +31,106 @@ def get_train_test():
     x_test = cv.transform(x_test)
 
     return x_train, x_test, y_train, y_test
+
+def _extract_text():
+
+    fields = ["Sentence", "Emotion", "Session", "Session"]
+    with open("data/text_data.csv", "a") as f:
+        write = csv.writer(f)
+        write.writerow(fields)
+
+    for i in range(1, 6):
+        directory = f"data/IEMOCAP_dataset/Session{i}/dialog/transcriptions"
+        directory1 = f"data/IEMOCAP_dataset/Session{i}/dialog/EmoEvaluation"
+
+        emotions = []
+
+        final = []
+        transcripts = []
+        ses_list_final = []
+        emo_ses_list = []
+
+        transcriptions = os.listdir(directory)
+        transcriptions.sort()
+
+        emoEvaluation = os.listdir(directory1)
+        emoEvaluation.sort()
+
+
+        for filename1 in emoEvaluation[3:]:
+            file_path = directory1 + "/" + filename1
+            file = open(file_path, "r")
+            lines = file.readlines()
+
+            for line in lines:
+                if line[0] == "[":
+                    emotions.append(line.split()[4])
+                    emo_ses_list.append(line.split()[3])
+
+        for filename in transcriptions:
+            m_list = []
+            f_list = []
+            ses_list_f = []
+            ses_list_m = []
+
+            file_path = directory + "/" + filename
+            file = open(file_path, "r")
+            transcript_lines = file.readlines()
+
+            for lines in transcript_lines:
+                lines = lines.split(":")
+
+                if len(lines[0]) > 10:
+
+                    if lines[0][-24] == "F" and lines[0][-23] != "X":
+                        f_list.append(lines[1][1:-1])
+                        ses_list_f.append(lines[0])
+
+                    elif lines[0][-24] == "M" and lines[0][-23] != "X":
+                        m_list.append(lines[1][1:-1])
+                        ses_list_m.append(lines[0])
+
+            transcripts += f_list + m_list
+            ses_list_final += ses_list_f + ses_list_m
+
+        for i in range(len(transcripts)):
+            final.append([transcripts[i], emotions[i],
+                        ses_list_final[i], emo_ses_list[i]])
+
+        rows = final
+
+        with open("data/text_data.csv", "a") as f:
+            write = csv.writer(f)
+            write.writerows(rows)
+
+def _unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+# Lowercase, trim, and remove non-letter characters
+def _normalizeString(s):
+    s = _unicodeToAscii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+    return s
+
+def make_text_features():
+    _extract_text()
+
+    data = pd.read_csv("data/text_data.csv")
+    transcripts = data['Sentence'].tolist()
+
+    for i in range(len(transcripts)):
+
+        transcripts[i] = transcripts[i].replace("'", "")
+        transcripts[i] = _normalizeString(transcripts[i])
+
+
+    data["Cleaned"] = transcripts
+
+    data.to_csv('data/text_data_clean.csv', index=False)
+
+if __name__ == '__main__':
+    make_text_features()
