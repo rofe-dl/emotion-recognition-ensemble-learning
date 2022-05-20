9ddd3f0c2e975c13d99232a4172da0da14bc4d7e
rofe-dl <rafidul258@gmail.com>
2022-05-20 18:27:57 +0600

    added feature figures for speech

--- process_dataset/old_code/trim.py
+++ process_dataset/old_code/trim.py
@@ -14,14 +14,14 @@ def main():
 
 def trim(file_name):
 
-    old_file_path = 'data/IEMOCAP_dataset_denoised/' + file_name
+    old_file_path = 'data/IEMOCAP_dataset/' + file_name
     with sf.SoundFile(old_file_path) as sound_file:
         sample_rate = sound_file.samplerate
 
     audio, sr = librosa.load(old_file_path, sr=sample_rate, mono=True)
     clip = librosa.effects.trim(audio, top_db=30)
 
-    path_to_new_file = 'data/IEMOCAP_dataset_denoised_trimmed/' + file_name
+    path_to_new_file = 'data/IEMOCAP_dataset_trimmed/' + file_name
     if not os.path.exists(os.path.dirname(path_to_new_file)):
         os.makedirs(os.path.dirname(path_to_new_file))
 
